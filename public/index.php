c5f7bfe4f30864ce03fbf4f11fc239483fd9cbc3
SomebirdFaust <faust.gaulard@gmail.com>
2024-05-29 09:19:29 +0200

    mise en place db

--- public/index.php
+++ public/index.php
@@ -1,15 +1,17 @@
 <?php
-header('Content-Type: application/json');
+// header('Content-Type: application/json');
 
-require_once __DIR__ . '/../src/Database.php';
-require_once __DIR__ . '/../src/Router.php';
-require_once __DIR__ . '/../src/Controller.php';
+// require_once __DIR__ . '/../src/Database.php';
+// require_once __DIR__ . '/../src/Router.php';
+// require_once __DIR__ . '/../src/Controller.php';
 
-$db = (new Database())->getConnection();
-$controller = new Controller($db);
-$router = new Router();
+// $db = (new Database())->getConnection();
+// $controller = new Controller($db);
+// $router = new Router();
 
-$router->addRoute('GET', '/items', [$controller, 'getItems']);
-$router->addRoute('POST', '/items', [$controller, 'createItem']);
+// $router->addRoute('GET', '/items', [$controller, 'getItems']);
+// $router->addRoute('POST', '/items', [$controller, 'createItem']);
 
-$router->handleRequest();
+// $router->handleRequest();
+
+echo'hello';
